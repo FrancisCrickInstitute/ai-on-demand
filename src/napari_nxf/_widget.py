698e7e6232b161fa07e34a603ef0d0617f0691fa
sea-shunned <seashunned@gmail.com>
2023-07-05 20:26:32 +0100

    Rewrite model config saving, default params, and widget handling

--- src/napari_nxf/_widget.py
+++ src/napari_nxf/_widget.py
@@ -4,8 +4,21 @@ from pathlib import Path
 import subprocess
 import time
 from typing import Union
+import yaml
 
-from qtpy.QtWidgets import QVBoxLayout, QHBoxLayout, QGridLayout, QPushButton, QWidget, QFileDialog, QLabel, QLineEdit, QScrollArea, QRadioButton, QGroupBox, QCheckBox
+from qtpy.QtWidgets import (
+    QVBoxLayout,
+    QHBoxLayout,
+    QGridLayout,
+    QPushButton,
+    QWidget,
+    QFileDialog,
+    QLabel,
+    QLineEdit,
+    QRadioButton,
+    QGroupBox,
+    QComboBox,
+)
 from qtpy.QtGui import QPixmap, QIntValidator, QDoubleValidator
 import qtpy.QtCore
 import numpy as np
@@ -16,13 +29,10 @@ import napari
 from napari.qt.threading import thread_worker
 from napari.utils.notifications import show_info
 
-from .model_params import MODEL_PARAMS, VALIDATORS
+from .model_params import MODEL_PARAMS, MODEL_VERSIONS
 
 # Available models, with displayable names
-MODELS = {
-    "unet": "UNet",
-    "sam": "Segment Anything"
-}
+MODELS = {"unet": "UNet", "sam": "Segment Anything"}
 # Specify which models are available for each task
 MODEL_DICT = {
     "mito": [k for k in MODELS if k != "sam"],
@@ -30,6 +40,7 @@ MODEL_DICT = {
     "ne": [k for k in MODELS if k != "sam"],
     "everything": ["sam"],
 }
+
 
 class AIOnDemand(QWidget):
     def __init__(self, napari_viewer: napari.Viewer):
@@ -41,7 +52,9 @@ class AIOnDemand(QWidget):
         self.selected_model = None
 
         # Set the path to watch for masks
-        self.mask_path = Path(__file__).parent / "nextflow/modules/models" / "sam_masks"
+        self.mask_path = (
+            Path(__file__).parent / "nextflow/modules/models" / "sam_masks"
+        )
 
         # Set overall layout for widget
         self.setLayout(QVBoxLayout())
@@ -49,7 +62,11 @@ class AIOnDemand(QWidget):
         # Add Crick logo at top for flavour
         self.logo_label = QLabel()
         logo = QPixmap(
-            str(Path(__file__).parent.parent.parent / "resources" / "CRICK_Brandmark_01_transparent.png")
+            str(
+                Path(__file__).parent.parent.parent
+                / "resources"
+                / "CRICK_Brandmark_01_transparent.png"
+            )
         ).scaledToHeight(150)
         self.logo_label.setPixmap(logo)
         self.logo_label.setAlignment(qtpy.QtCore.Qt.AlignCenter)
@@ -77,7 +94,12 @@ class AIOnDemand(QWidget):
         self.task_buttons = {}
         for name, label in zip(
             ["mito", "er", "ne", "everything"],
-            ["Mitochondria", "Endoplasmic Reticulum", "Nuclear Envelope", "Everything!"]
+            [
+                "Mitochondria",
+                "Endoplasmic Reticulum",
+                "Nuclear Envelope",
+                "Everything!",
+            ],
         ):
             btn = QRadioButton(label)
             btn.setEnabled(True)
@@ -150,11 +172,15 @@ class AIOnDemand(QWidget):
             self.model_widget = self.model_param_widgets[model_name]
         # Otherwise construct it
         else:
-            self.model_widget = self._create_model_param_widget(model_name, param_dict)
+            self.model_widget = self._create_model_param_widget(
+                model_name, param_dict
+            )
             self.model_param_widgets[model_name] = self.model_widget
         # Set the collapsible box to contain the params for this model
         self.model_param_box.addWidget(self.model_widget)
-        self.model_param_box._toggle_btn.setText(f"{MODELS[model_name]} parameters:")
+        self.model_param_box._toggle_btn.setText(
+            f"{MODELS[model_name]} parameters:"
+        )
 
     def _create_model_param_widget(self, model_name, param_dict):
         # Create a widget for the model parameters
@@ -162,35 +188,56 @@ class AIOnDemand(QWidget):
         model_layout = QGridLayout()
         # Create container for model parameters
         self.model_param_dict[model_name] = {}
-        for i, (param, _, value, validator_type) in enumerate(param_dict):
+        # Add dropdown of model checkpoints/types if available
+        self._list_model_checkpoints(model_name, model_layout)
+        # Initialize row counter in grid layout
+        i = 1
+        # Add the default model parameters
+        for label, model_param in param_dict.items():
             # Create labels for each of the model parameters
-            param_label = QLabel(f"{param}:")
+            param_label = QLabel(f"{label}:")
+            param_label.setToolTip(model_param.tooltip)
             model_layout.addWidget(param_label, i, 0)
             # Add the default model parameter
             param_value = QLineEdit()
-            param_value.setText(str(value))
-            # Add validator
-            validator_cls = VALIDATORS[validator_type]
-            validator = validator_cls()
-            if isinstance(validator, QIntValidator):
-                validator.setRange(0, 9999)
-            elif isinstance(validator, QDoubleValidator):
-                validator.setRange(0.0, 1.0)
-                validator.setDecimals(4)
-            param_value.setValidator(validator)
+            param_value.setText(str(model_param.default))
             model_layout.addWidget(param_value, i, 1)
             # Store for later retrieval when saving the config
-            self.model_param_dict[model_name][param] = {
+            self.model_param_dict[model_name][label] = {
                 "label": param_label,
-                "value": param_value
+                "value": param_value,
             }
+            i += 1
+        # Tighten up margins and set layout
         model_layout.setContentsMargins(0, 0, 0, 0)
         model_widget.setLayout(model_layout)
         return model_widget
 
+    def _list_model_checkpoints(self, model_name, model_layout):
+        model_types = MODEL_VERSIONS[model_name]
+        # If no types available, just add a default
+        if model_types is None:
+            model_types = ["default"]
+        # Create a label for the dropdown
+        param_label = QLabel("Model type:")
+        param_label.setToolTip(
+            "Select the model type to use. If only 'default' is available, then no model variants are available."
+        )
+        # Create the dropdown box
+        model_type_box = QComboBox()
+        model_type_box.addItems(model_types)
+        model_type_box.setCurrentIndex(0)
+        # Insert into dict for later retrieval
+        self.model_param_dict[model_name]["model_type"] = {
+            "label": param_label,
+            "value": model_type_box,
+        }
+        # Add to layout
+        model_layout.addWidget(param_label, 0, 0)
+        model_layout.addWidget(model_type_box, 0, 1)
+
     def update_model_box(self):
-        '''The model box updates according to what's defined for each task
-        '''
+        """The model box updates according to what's defined for each task"""
         # Find out which button was pressed
         for task_name, task_btn in self.task_buttons.items():
             if task_btn.isChecked():
@@ -231,7 +278,9 @@ class AIOnDemand(QWidget):
         # Create a button to navigate to a directory to take images from
         self.dir_btn = QPushButton("Select directory")
         self.dir_btn.clicked.connect(self.browse_directory)
-        self.dir_btn.setToolTip("Select folder/directory of images to use as input to the model.")
+        self.dir_btn.setToolTip(
+            "Select folder/directory of images to use as input to the model."
+        )
         self.dir_layout.addWidget(self.dir_btn)
         # Add an output to show the counts
         self.img_counts = QLabel()
@@ -243,7 +292,9 @@ class AIOnDemand(QWidget):
         # Add a button for viewing the images within napari
         # Optional as potentially taxing, and not necessary
         self.view_img_btn = QPushButton("View selected images")
-        self.view_img_btn.setToolTip("Load selected images into napari to view.")
+        self.view_img_btn.setToolTip(
+            "Load selected images into napari to view."
+        )
         self.view_img_btn.clicked.connect(self.view_images)
         self.dir_layout.addWidget(self.view_img_btn)
         # Sort out layout and add to main widget
@@ -260,9 +311,7 @@ class AIOnDemand(QWidget):
 
     def browse_directory(self):
         result = QFileDialog.getExistingDirectory(
-            self,
-            caption="Select image directory",
-            directory=None
+            self, caption="Select image directory", directory=None
         )
         # If a new directory is selected, reset the load button text
         if result != self.images_dir:
@@ -274,17 +323,17 @@ class AIOnDemand(QWidget):
             self._count_files()
 
     def type_directory(self):
-        '''Allow for user to type a directory?
+        """Allow for user to type a directory?
 
         Will require checking that the path is valid, and using the ToolTip if it's not
         (or a similar pop-up/warning object) to raise the issue.
-        '''
+        """
         return NotImplementedError
 
     def _count_files(self):
-        '''Function to extract all the files in a given path,
+        """Function to extract all the files in a given path,
         and return a count (broken down by extension)
-        '''
+        """
         txt = ""
         # Get all the files in the given path
         self.all_img_files = list(Path(self.images_dir).glob("*"))
@@ -295,7 +344,7 @@ class AIOnDemand(QWidget):
         if len(ext_counts) > 1:
             # Nicely format the list of files and their extensions
             for i, (ext, count) in enumerate(ext_counts):
-                if i == (len(ext_counts)-1):
+                if i == (len(ext_counts) - 1):
                     txt += f"and {count} {ext}"
                 else:
                     txt += f"{count} {ext}, "
@@ -311,10 +360,17 @@ class AIOnDemand(QWidget):
             return
         # Reset counter
         self.load_img_counter = 0
+
         # Create separate thread worker to avoid blocking
-        @thread_worker(connect={"returned": self._add_image, "finished": self._reset_view_btn})
+        @thread_worker(
+            connect={
+                "returned": self._add_image,
+                "finished": self._reset_view_btn,
+            }
+        )
         def _load_image(fpath):
             return skimage.io.imread(fpath), fpath
+
         # Load each image in a separate thread
         for fpath in self.all_img_files:
             _load_image(fpath)
@@ -326,9 +382,13 @@ class AIOnDemand(QWidget):
         img, fpath = res
         self.viewer.add_image(img, name=fpath.name)
         self.load_img_counter += 1
-        self.view_img_btn.setText(f"Loading...({self.load_img_counter}/{len(self.all_img_files)} images loaded).")
+        self.view_img_btn.setText(
+            f"Loading...({self.load_img_counter}/{len(self.all_img_files)} images loaded)."
+        )
         if self.load_img_counter == len(self.all_img_files):
-            self.view_img_btn.setText(f"All ({self.load_img_counter}/{len(self.all_img_files)}) images loaded.")
+            self.view_img_btn.setText(
+                f"All ({self.load_img_counter}/{len(self.all_img_files)}) images loaded."
+            )
 
     def _reset_view_btn(self):
         self.view_img_btn.setEnabled(True)
@@ -343,15 +403,14 @@ class AIOnDemand(QWidget):
             try:
                 img_shape = self.viewer.layers[f"{fpath.name}"].data.shape
             except KeyError:
-                img_shape = (1000,1000)
+                img_shape = (1000, 1000)
             # Set the name following convention
             name = f"{fpath.stem}_masks"
             # Add a Labels layer for this file
             self.viewer.add_labels(
-                np.zeros(img_shape, dtype=int),
-                name=name,
-                visible=False
+                np.zeros(img_shape, dtype=int), name=name, visible=False
             )
+
         # NOTE: Wrapper as self/class not available at runtime
         @thread_worker(connect={"yielded": self.update_masks})
         def _watch_mask_files(self):
@@ -365,7 +424,9 @@ class AIOnDemand(QWidget):
                 current_files = list(self.mask_path.glob("*.npy"))
                 if set(self.mask_fpaths) != set(current_files):
                     # Get the new files only
-                    new_files = [i for i in current_files if i not in self.mask_fpaths]
+                    new_files = [
+                        i for i in current_files if i not in self.mask_fpaths
+                    ]
                     # Update file list and yield the difference
                     self.mask_fpaths = current_files
                     if new_files:
@@ -373,13 +434,16 @@ class AIOnDemand(QWidget):
                 # Sleep until next check
                 time.sleep(2)
                 # Check all masks contain data for all slices
-                masks_finished = [Path(i).stem[-3:] == "all" for i in current_files]
+                masks_finished = [
+                    Path(i).stem[-3:] == "all" for i in current_files
+                ]
                 # Get how many mask files there should be
                 num_images = len(self.all_img_files)
                 # If all images have complete masks, deactivate watcher
                 if all(masks_finished) and (len(masks_finished) == num_images):
                     print("Deactivating watcher...")
                     self.watcher_enabled = False
+
         # Call the nested function
         _watch_mask_files(self)
 
@@ -396,7 +460,7 @@ class AIOnDemand(QWidget):
             # Insert mask data
             label_layer.data = mask_arr
             label_layer.visible = True
-            slice_num = split_names[-1].replace("_","")
+            slice_num = split_names[-1].replace("_", "")
             # Switch viewer to latest slice
             if slice_num == "all":
                 slice_num = label_layer.data.shape[0]
@@ -409,7 +473,9 @@ class AIOnDemand(QWidget):
         # Create a button to navigate to a directory to take images from
         self.nxf_btn = QPushButton("Run Pipeline!")
         self.nxf_btn.clicked.connect(self.run_pipeline)
-        self.nxf_btn.setToolTip("Run the pipeline with the chosen organelle(s), model, and images.")
+        self.nxf_btn.setToolTip(
+            "Run the pipeline with the chosen organelle(s), model, and images."
+        )
         self.nxf_layout.addWidget(self.nxf_btn)
         self.layout().addLayout(self.nxf_layout)
 
@@ -420,17 +486,50 @@ class AIOnDemand(QWidget):
             output.write("\n".join([str(i) for i in self.all_img_files]))
 
     def create_nextflow_params(self):
+        config_path, model_type = self.get_model_config()
         nxf_params = {}
         nxf_params["img_dir"] = str(self.img_list_fpath)
-        nxf_params["model_config"] = self.get_model_config()
         nxf_params["model"] = self.selected_model
+        nxf_params["model_config"] = config_path
+        nxf_params["model_type"] = model_type
         nxf_params["task"] = self.selected_task
         # TODO: Implement profiles for this to configure SLURM
         nxf_params["executor"] = "Local"
         return nxf_params
 
     def get_model_config(self):
-        return str(Path.cwd())
+        # Get the current dictionary of widgets for selected model
+        model_dict_orig = self.model_param_dict[self.selected_model]
+        # Get the relevant default params for this model
+        default_params = MODEL_PARAMS[self.selected_model]
+        # Reformat the dict to pipe into downstream model run scripts
+        model_dict = {}
+        # Extract params from model param widgets
+        for param_name, sub_dict in model_dict_orig.items():
+            if isinstance(sub_dict["value"], QLineEdit):
+                param_value = sub_dict["value"].text()
+            elif isinstance(sub_dict["value"], QComboBox):
+                param_value = sub_dict["value"].itemData(
+                    sub_dict["value"].currentIndex()
+                )
+            else:
+                raise NotImplementedError
+            # Extract the original/intended dtype and cast what's in the box
+            if param_name != "model_type":
+                orig_dtype = default_params[param_name].dtype
+                model_dict[param_name] = orig_dtype(param_value)
+        # Extract the model type
+        model_type = model_dict_orig["model_type"]["value"].currentText()
+        # Define save path for the model config
+        config_dir = Path(__file__).parent / "nextflow" / "configs"
+        config_dir.mkdir(parents=True, exist_ok=True)
+        model_config_fpath = (
+            config_dir / f"{self.selected_model}-{model_type}_config.yaml"
+        )
+        # Save the yaml config
+        with open(model_config_fpath, "w") as f:
+            yaml.dump(model_dict, f)
+        return model_config_fpath, model_type
 
     def run_pipeline(self):
         # Start with some error checking to ensure that everything has been properly specified
@@ -453,7 +552,12 @@ class AIOnDemand(QWidget):
         # Start the mask file watcher
         self.watch_mask_files()
 
-        @thread_worker(connect={"returned": self._pipeline_finish, "errored": self._pipeline_fail})
+        @thread_worker(
+            connect={
+                "returned": self._pipeline_finish,
+                "errored": self._pipeline_fail,
+            }
+        )
         def _run_pipeline(self, nxf_params):
             # Update the button to signify it's running
             self.nxf_btn.setText("Running Pipeline...")
@@ -466,10 +570,7 @@ class AIOnDemand(QWidget):
             for k, v in nxf_params.items():
                 exec_str += f" --{k}={v}"
             # Run the pipeline!
-            subprocess.run(
-                exec_str,
-                shell=True
-            )
+            subprocess.run(exec_str, shell=True)
             # TODO: Have some error-handling/polling
 
         _run_pipeline(self, nxf_params)
