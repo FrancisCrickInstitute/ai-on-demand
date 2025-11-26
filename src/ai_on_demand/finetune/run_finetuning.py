65168f60ede0a26253f8884f5819d260557d04d6
n4hm3 <nahmedshaon@gmail.com>
2025-11-26 17:08:30 +0000

    adding data augmentation to fix shape issue

--- src/ai_on_demand/finetune/run_finetuning.py
+++ src/ai_on_demand/finetune/run_finetuning.py
@@ -5,9 +5,57 @@ from .finetuning_utils import PanopticLoss
 from torch.utils.data import DataLoader
 import numpy as np
 
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
 
 # from sklearn import measure
 # from skimage import io
+
+augmentations = sorted(
+    name
+    for name in A.__dict__
+    if callable(A.__dict__[name])
+    and not name.startswith("__")
+    and name[0].isupper()
+)
+
+augmentations_dict = [
+    {"aug": "RandomScale", "scale_limit": [-0.9, 1]},
+    {
+        "aug": "PadIfNeeded",
+        "min_height": 256,
+        "min_width": 256,
+        "border_mode": 0,
+    },
+    {"aug": "RandomCrop", "height": 256, "width": 256},
+    {"aug": "Rotate", "limit": 180, "border_mode": 0},
+    {
+        "aug": "RandomBrightnessContrast",
+        "brightness_limit": 0.3,
+        "contrast_limit": 0.3,
+    },
+    {"aug": "HorizontalFlip"},
+    {"aug": "VerticalFlip"},
+]
+
+# set the training image augmentations
+norms = {"mean": 0.57571, "std": 0.12765}
+aug_string = []
+dataset_augs = []
+for aug_params in augmentations_dict:
+    aug_name = aug_params["aug"]
+
+    assert (
+        aug_name in augmentations or aug_name == "CopyPaste"
+    ), f"{aug_name} is not a valid albumentations augmentation!"
+
+    aug_string.append(aug_params["aug"])
+    del aug_params["aug"]
+    dataset_augs.append(A.__dict__[aug_name](**aug_params))
+
+aug_string = ",".join(aug_string)
+
+tfs = A.Compose([*dataset_augs, A.Normalize(**norms), ToTensorV2()])
 
 
 def finetune(config):
@@ -27,7 +75,7 @@ def finetune(config):
     # allows falsy values - "", None
     data_cls = SingleClassInstanceDataset
     train_dataset = data_cls(
-        train_dir, weight_gamma=0.7
+        train_dir, transforms=tfs, weight_gamma=0.7
     )  # what does weight gamma mean?
 
     train_loader = DataLoader(
