0667d12ed869fb0d4ee9bad8e8ae66ca696af405
n4hm3 <nahmedshaon@gmail.com>
2026-01-12 15:04:58 +0000

    inference test mitonet

--- src/ai_on_demand/_tests/inference_test.py
+++ src/ai_on_demand/_tests/inference_test.py
@@ -1,5 +1,8 @@
 import pytest
-from napari import Viewer
+from napari import Viewer, run
+from qtpy.QtCore import QTimer
+from skimage import io
+import numpy as np
 
 
 @pytest.fixture(scope="module")
@@ -9,7 +12,7 @@ def napari_viewer():
     viewer.close()
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture
 def inference_widget(napari_viewer):
     dock_widget, plugin_widget = napari_viewer.window.add_plugin_dock_widget(
         "ai-on-demand", "Inference"
@@ -17,25 +20,60 @@ def inference_widget(napari_viewer):
     return plugin_widget
 
 
-def test_inference_workflow(inference_widget):
-    inference_widget.subwidgets["data"].update_file_count(
+def test_inference_workflow(napari_viewer, inference_widget):
+    max_time_duration = 60000  # msec = 60seconds
+    plugin_widget = inference_widget
+
+    plugin_widget.subwidgets["data"].update_file_count(
         paths=[
-            "/Users/ahmedn/Work/aiod_test_images/example_mito.tif",
+            "-",
         ]
     )
-    inference_widget.subwidgets["data"].view_images()
+
+    plugin_widget.subwidgets["data"].view_images()
 
     # Select task
-    inference_widget.subwidgets["task"].task_buttons["mito"].click()
+    plugin_widget.subwidgets["task"].task_buttons["mito"].click()
 
     # overwrite
-    inference_widget.subwidgets["nxf"].overwrite_btn.setChecked(True)
-
-    # trigger the run (commented in debug.py) - won't work unless the images have been loaded
-    # inference_widget.subwidgets["nxf"].nxf_run_btn.click()
+    plugin_widget.subwidgets["nxf"].overwrite_btn.setChecked(True)
 
     # Assert that the widget is in a ready state (example: check overwrite is set)
-    assert inference_widget.subwidgets["nxf"].overwrite_btn.isChecked()
+    assert plugin_widget.subwidgets["nxf"].overwrite_btn.isChecked()
 
+    def on_pipeline_finished():
+        print("pipeline finished!")
+        output_mask = napari_viewer.layers[
+            "2D_mito_masks_mito-empanada-MitoNet-v1-43e45ccf"
+        ].data
 
-import pytest
+        reference_mask = io.imread(
+            "./test_images/masks/2D_mitonet_V1_masks.tif"
+        )
+
+        assert np.array_equal(reference_mask, output_mask)
+        napari_viewer.close()
+        run.quit()
+
+    def on_pipeline_failed():
+        pytest.fail("Inference pipeline failed")
+        napari_viewer.close()
+        run.quit()
+
+    plugin_widget.subwidgets["nxf"].pipeline_finished.connect(
+        on_pipeline_finished
+    )
+    plugin_widget.subwidgets["nxf"].pipeline_failed.connect(on_pipeline_failed)
+
+    def run_pipeline():
+        plugin_widget.subwidgets["nxf"].nxf_run_btn.click()
+        print("running the pipeline!")
+
+    def timeout():
+        pytest.fail(f"Timeout duration:{max_time_duration}")
+
+    QTimer.singleShot(
+        3000, run_pipeline
+    )  # possible issue if the image didn't load in 3 seconds? better to have this on an emit?
+    QTimer.singleShot(max_time_duration, timeout)  # time out after 1 minute
+    run()
